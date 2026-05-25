2c94b039c28ca46803e5f5d99c0e3effb744d261
Yonggang Luo <luoyonggang@gmail.com>
2026-05-26 02:28:31 +0800

    msys2-runtime: Add patches that support for build as cygwin1.dll or msys-2.0.dll
    migrating msys2-runtime to cygwin
    
    msys2-runtime commit-id for cygwin1.dll is from:
    https://github.com/lygstate/msys2-runtime/commit/fd5ce305d44723feb3e60c927295d5973ee7692b
    
    The commit-id for msys-2.0.dll and cygwin1.dll should not be the same.
    So that they can call each other properly
    
    Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>

---
+++ msys2-runtime/check-bootstrap.sh
@@ -0,0 +1,15 @@
+
+check_bootstrap() {
+  if [[ -z "$MSYS_BOOTSTRAP_STAGE" ]]; then
+    ldd_output="$(ldd /usr/bin/ldd 2>&1)"
+    SUB="msys-2.0.dll"
+    if [[ "$ldd_output" == *"$SUB"* ]]; then
+        MSYS_BOOTSTRAP_STAGE="stage_origin"
+    else
+        MSYS_BOOTSTRAP_STAGE="stage3"
+    fi
+  fi
+  export MSYS_BOOTSTRAP_STAGE
+}
+
+check_bootstrap
